3c2239e8a5e915c8c98ec9c7d4da050f3ee5b747
Fatih Ka. <xybydy@gmail.com>
2025-08-20 10:12:51 +0300

    x

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -43,13 +43,13 @@ sed -i "s/procd-ujail//" include/target.mk
 
 sed -i "s/^.*vermagic$/\techo '1' > \$(LINUX_DIR)\/.vermagic/" include/kernel-defaults.mk
 
-status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
-echo "$status"
-while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
-	echo "wait 5s"
-	sleep 5
-	status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
-done
+# status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
+# echo "$status"
+# while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
+# 	echo "wait 5s"
+# 	sleep 5
+# 	status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
+# done
 
 mv -f feeds/kiddin9/r81* tmp/
 
