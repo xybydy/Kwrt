09d35871680107e8f2372d5cca2a0d3ba7945ad2
Fatih Ka. <xybydy@gmail.com>
2025-10-11 13:19:43 +0300

    f rev

--- test/f.sh
+++ test/f.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install build-essential clang flex bison g++ gawk \
+gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
+python3-setuptools rsync swig unzip zlib1g-dev file wget \
+llvm python3-pyelftools libpython3-dev aria2 jq qemu-utils ccache rename \
+libelf-dev device-tree-compiler libgmp3-dev libmpc-dev libfuse-dev
+sudo -E apt-get -qq autoremove --purge
+    sudo -E apt-get -qq clean
+
+
+
 function git_clone_path() {
   trap 'rm -rf "$tmpdir"' EXIT
   branch="$1" rurl="$2" mv="$3"
