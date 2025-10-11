ac6354339f70c2a1d1ea1fb63c1e8dbcc315109f
Fatih Ka. <xybydy@gmail.com>
2025-10-11 13:42:01 +0300

    Update f.sh

--- test/f.sh
+++ test/f.sh
@@ -8,7 +8,7 @@ python3-setuptools rsync swig unzip zlib1g-dev file wget \
 llvm python3-pyelftools libpython3-dev aria2 jq qemu-utils ccache rename \
 libelf-dev device-tree-compiler libgmp3-dev libmpc-dev libfuse-dev
 sudo -E apt-get -qq autoremove --purge
-    sudo -E apt-get -qq clean
+sudo -E apt-get -qq clean
 
 
 
