1f30f52d09f5d9ef4e58b8a61f05f4658fcc787d
Fatih Ka. <xybydy@gmail.com>
2025-10-11 13:29:56 +0300

    x

--- test/f.sh
+++ test/f.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 
 sudo -E apt-get -qq update
 sudo -E apt-get -qq install build-essential clang flex bison g++ gawk \
-gcc-multilib g++-multilib gettext git libncurses5-dev libssl-dev \
+gettext git libncurses5-dev libssl-dev \
 python3-setuptools rsync swig unzip zlib1g-dev file wget \
 llvm python3-pyelftools libpython3-dev aria2 jq qemu-utils ccache rename \
 libelf-dev device-tree-compiler libgmp3-dev libmpc-dev libfuse-dev
